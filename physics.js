919debb0fbe124c9c784e8182daf8674a164074e
sealbaby <sealbaby@users.noreply.github.com>
2025-08-13 20:57:36 +0100

    Update physics.js

--- physics.js
+++ physics.js
@@ -1,39 +1,42 @@
-(function(){
+(function () {
   'use strict';
 
-  function aabbIntersect(a,b){
-    return a.x < b.x+b.w && a.x+a.w > b.x && a.y < b.y+b.h && a.y+a.h > b.y;
+  function aabbIntersect(a, b) {
+    return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.y + a.h > b.y;
   }
 
-  function resolveWorldCollision(e, GROUND_Y){
-    e.onGround=false;
-    if(e.y + e.h >= GROUND_Y){
-      e.y = GROUND_Y - e.h;
-      e.vy = 0;
-      e.onGround = true;
+  function resolveWorldCollision(obj, GROUND_Y) {
+    if (obj.y + obj.h >= GROUND_Y) {
+      obj.y = GROUND_Y - obj.h;
+      obj.vy = 0;
+      obj.onGround = true;
+    } else {
+      obj.onGround = false;
     }
-    if(e.y < 0){ e.y=0; if(e.vy<0) e.vy=0; }
   }
 
-  function resolveStructuresCollision(e, solids){
-    for(const s of solids){
-      const r={x:e.x,y:e.y,w:e.w,h:e.h};
-      if(!aabbIntersect(r,s)) continue;
-      const dx1 = (s.x + s.w) - r.x;
-      const dx2 = (r.x + r.w) - s.x;
-      const dy1 = (s.y + s.h) - r.y;
-      const dy2 = (r.y + r.h) - s.y;
-      const minX = Math.min(dx1, dx2);
-      const minY = Math.min(dy1, dy2);
-      if(minX < minY){
-        if(dx1 < dx2){ e.x = s.x + s.w; } else { e.x = s.x - e.w; }
-        e.vx = 0;
-      } else {
-        if(dy1 < dy2){ e.y = s.y + s.h; if(e.vy<0) e.vy=0; }
-        else { e.y = s.y - e.h; e.vy=0; e.onGround=true; }
+  function resolveStructuresCollision(obj, rects) {
+    // simple vertical resolution first
+    for (let i = 0; i < rects.length; i++) {
+      const r = rects[i];
+      if (!(obj.x < r.x + r.w && obj.x + obj.w > r.x)) continue;
+      // coming down onto top
+      if (obj.y + obj.h > r.y && obj.prevY + obj.h <= r.y) {
+        obj.y = r.y - obj.h;
+        obj.vy = 0;
+        obj.onGround = true;
       }
     }
+    // horizontal push-out
+    for (let i = 0; i < rects.length; i++) {
+      const r = rects[i];
+      const inter = !(obj.x + obj.w <= r.x || obj.x >= r.x + r.w || obj.y + obj.h <= r.y || obj.y >= r.y + r.h);
+      if (!inter) continue;
+      if (obj.x + obj.w / 2 < r.x + r.w / 2) obj.x = r.x - obj.w - 0.01;
+      else obj.x = r.x + r.w + 0.01;
+      obj.vx = 0;
+    }
   }
 
-  window.Physics={aabbIntersect, resolveWorldCollision, resolveStructuresCollision};
+  window.Physics = { aabbIntersect, resolveWorldCollision, resolveStructuresCollision };
 })();
